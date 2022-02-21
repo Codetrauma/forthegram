360764fc9c214f2cf488afbaf34a6470f36ee25a
Tanner Shaw <traumaaa@gmail.com>
2022-02-21 15:23:22 -0800

    styling and fixed comment delete button

--- react-app/src/components/UserProfile/UserProfile.js
+++ react-app/src/components/UserProfile/UserProfile.js
@@ -11,11 +11,15 @@ function UserProfile() {
   const dispatch = useDispatch();
   const id = useParams();
 
+  const emailPattern = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+  const isEmail = field => emailPattern.test(field)
+
   const [showEditForm, setShowEditForm] = useState(false);
   const [fullname, setFullname] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [description, setDescription] = useState('');
+  const [errors, setErrors] = useState([])
 
   const sessionUser = useSelector(state => state.session.user);
 
@@ -35,18 +39,28 @@ function UserProfile() {
   }, [dispatch]);
 
   const handleProfileSubmit = (e) => {
+    let newErrors = [];
     e.preventDefault();
-    const userInfo = {
-      id: sessionUser.id,
-      full_name: fullname,
-      username: username,
-      email: email,
-      description: description,
+    if (!isEmail(email)) {
+      newErrors.push('Please enter a valid email.')
+      setErrors(newErrors)
     }
-    dispatch(updateUserInfo(userInfo));
+    else {
+      const userInfo = {
+        id: sessionUser.id,
+        full_name: fullname,
+        username: username,
+        email: email,
+        description: description,
+      }
+      dispatch(updateUserInfo(userInfo));
+      setShowEditForm(!showEditForm);
+    }
+  }
+  const handleCancel = (e) => {
+    e.preventDefault();
     setShowEditForm(!showEditForm);
   }
-
 
   return (
     <div className='profile-wrapper'>
@@ -55,36 +69,39 @@ function UserProfile() {
           <div className='profile-header'>
             <img src={user[0]?.picture} height='200' className='user-profile-picture-profile' />
             <div className='profile-info'>
-            <h1>{user[0]?.username}</h1>
-            <h4>Followers</h4>
-            <h4>Following</h4>
-            <h4>{userPosts.length} Posts</h4>
+              <h1>{user[0]?.username}</h1>
+              <h4>Followers</h4>
+              <h4>Following</h4>
+              <h4>{userPosts.length} Posts</h4>
             </div>
           </div>
         </div>
         <div className='profile-info-container'>
+          <h3>{user[0]?.fullname}</h3>
+          {sessionUser.id === user[0]?.id && !showEditForm ? <button className='edit-profile-button' onClick={() => setShowEditForm(!showEditForm)}>Edit Profile</button> : <></>}
           <div className='profile-fullname'>
-            <h3>{user[0]?.fullname}</h3>{sessionUser.id === user[0]?.id ? <button className='edit-profile-button' onClick={() => setShowEditForm(!showEditForm)}>Edit Profile</button> : <></>}
             {showEditForm && (
-              <div className='edit-profile-form'>
-                <form className='edit-profile-form' onSubmit={handleProfileSubmit}>
-                  <input type='text' placeholder='Full Name' onChange={e => setFullname(e.target.value)}/>
-                  <input type='text' placeholder='Username' onChange={e => setUsername(e.target.value)}/>
-                  <input type='text' placeholder='Email' onChange={e => setEmail(e.target.value)}/>
-                  <input type='text' placeholder='Description' onChange={e => setDescription(e.target.value)}/>
-                  <button type='submit' value={user[0]?.id}>Save</button>
+              <div className='edit-profile-form-wrapper'>
+                <form className='edit-profile-form'>
+                  {errors.map(error => <p className='error-message'>{error}</p>)}
+                  <input type='text' className='edit-profile-inputs' placeholder='Full Name' onChange={e => setFullname(e.target.value)} />
+                  <input type='text' className='edit-profile-inputs' placeholder='Username' onChange={e => setUsername(e.target.value)} />
+                  <input type='email' className='edit-profile-inputs' placeholder='Email' onChange={e => setEmail(e.target.value)} />
+                  <textarea type='text'className='edit-profile-inputs-textarea' placeholder='Description' onChange={e => setDescription(e.target.value)} />
+                  <button type='submit' onClick={handleProfileSubmit} className='save-button' value={user[0]?.id}>Save</button>
+                  <button className='save-button' onClick={handleCancel}>Cancel</button>
                 </form>
               </div>
             )}
             <p>{user[0]?.description}</p>
           </div>
         </div>
-          <div className='divider'></div>
+        <div className='divider'></div>
         <div className='profile-post-wrapper'>
           <div className='profile-post-container'>
             {userPosts.map(userPost => (
               <SinglePostModal post={userPost} key={userPost.id} />
-                // <img value={userPost.id} className='profile-posts' src={userPost?.photos[0]?.photo} height='200' className='user-profile-post' />
+              // <img value={userPost.id} className='profile-posts' src={userPost?.photos[0]?.photo} height='200' className='user-profile-post' />
             ))}
           </div>
         </div>
