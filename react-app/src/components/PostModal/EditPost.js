9bb2859221cf65fe42f54e680863f09878f76fee
Tanner Shaw <traumaaa@gmail.com>
2022-02-24 15:48:19 -0800

    edit caption error handling

--- react-app/src/components/PostModal/EditPost.js
+++ react-app/src/components/PostModal/EditPost.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { loadAllPosts, updatePost } from '../../store/posts';
 import { useDispatch } from 'react-redux';
 
@@ -9,7 +9,15 @@ const EditCaption = ({ posts, setShowModal }) => {
   localStorage.setItem('caption', posts.caption)
 
   const [caption, setCaption] = useState(localStorage.getItem('caption'))
-  const [errors, setErrors] = useState('')
+  const [errors, setErrors] = useState([])
+
+  useEffect(() => {
+    const newErrors = [];
+    if (caption.length > 80) {
+      newErrors.push('Caption is too long');
+    }
+    setErrors(newErrors);
+  }, [caption])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,15 +31,16 @@ const EditCaption = ({ posts, setShowModal }) => {
     setShowModal(false);
   }
 
-    return (
-      <div className='form-wrapper'>
-        <h1>Edit Caption</h1>
-        <form className='post-modal-form'>
-          <textarea className='post-textarea' rows='7' cols='40' value={caption} onChange={(e) => setCaption(e.target.value)} />
-          <button  className='post-modal-submit' type="submit" onClick={handleSubmit}>Submit</button>
-        </form>
-      </div>
-    )
+  return (
+    <div className='form-wrapper'>
+      <h1>Edit Caption</h1>
+      {errors.map((error, i) => <li className='errors' key={i}>{error}</li>)}
+      <form className='post-modal-form'>
+        <textarea placeholder='Caption (Optional)' className='post-textarea' rows='7' cols='40' value={caption} onChange={(e) => setCaption(e.target.value)} />
+        <button disabled={caption.length > 80 ? true : false} className='post-modal-submit' type="submit" onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default EditCaption
