9cb1be9c7b57b4e8a1e96d826d26202a257a247c
Tanner Shaw <traumaaa@gmail.com>
2022-02-24 16:01:46 -0800

    fixed edit comment

--- react-app/src/components/dashboard/Comments.js
+++ react-app/src/components/dashboard/Comments.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateComment, removeComment } from '../../store/comments';
 import { loadAllPosts } from '../../store/posts';
@@ -7,13 +7,27 @@ const Comments = ({ comments }) => {
   const dispatch = useDispatch();
 
   const sessionUser = useSelector(state => state.session.user);
-  
+
   localStorage.setItem('comment', comments.comment);
 
 
   const [showEditForm, setShowEditForm] = useState(false);
   const [errors, setErrors] = useState([]);
   const [comment, setComment] = useState(localStorage.getItem('comment'))
+
+  useEffect(() => {
+    const newErrors = [];
+    if (comment.length > 80) {
+      newErrors.push('Comment is too long');
+    }
+    if (comment.length < 1 && comment.length) {
+      newErrors.push('Comment is too short');
+    }
+    if (comment.indexOf(' ') === 0) {
+      newErrors.push('Comment cannot start with a space');
+    }
+    setErrors(newErrors);
+  }, [comment])
 
 
   const handleDelete = async (e) => {
@@ -45,14 +59,14 @@ const Comments = ({ comments }) => {
           <form className='edit-comment-form'>
             <input type='text' value={comment} onChange={e => setComment(e.target.value)} />
             <button disabled={comment?.length <= 0 || comment?.length > 80 ? true : false} type='submit' value={comments.id} onClick={handleEdit}>Submit</button>
-            {errors.map(error => (
-              <p key={error} className='comment-error'>{error}</p>
-            ))}
           </form>
         )
-        }
-        <p hidden={showEditForm === true ? true : false} className='single-comment'>{comments.comment}</p>
+      }
+        {!showEditForm ? <p className='single-comment'>{comments.comment}</p> : <></>}
         <div className='comment-button-container'>
+      {showEditForm ? errors.map(error => (
+        <p key={error} className='errors'>{error}</p>
+      )) : <></>}
           {sessionUser?.id === comments.user_id ? <button className='comment-buttons' type='submit' value={comments.id} onClick={(e) => setShowEditForm(!showEditForm)}>Edit</button> : <></>}
           {!showEditForm && sessionUser?.id === comments.user_id ? <button className='comment-buttons-delete' type='submit' value={comments.id} onClick={handleDelete}>X</button> : <></>}
         </div>
